837ea06411eb0e544a644e793488f55278f06cb3
rishabh <rishabh.jain9191@gmail.com>
2017-04-01 00:36:11 +0530

    using mongoose and pagination

---
+++ db/connect.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://root:root@ds147480.mlab.com:47480/rishabh');
+
+module.exports = mongoose;
--- module/MarcoPoloService.js
+++ module/MarcoPoloService.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var MarcoPoloData = require('../schema/MarcoPoloData');
 /**
  * @module MarcoPolo 
  */
@@ -7,7 +8,7 @@ var fs = require('fs');
 'use strict';
 
 const START = 1;
-const END = 1000000;
+const END = 10000;
 const NO_LIMIT_IN_LINE = 1000;
 const NO_OF_LINE = (END - START + 1) / NO_LIMIT_IN_LINE;
 
@@ -29,21 +30,21 @@ var Marco_Polo = {
 					number.push(i);
 			}
 			
-			resolve(number);
+			resolve({range: start + '-' + end, result: number});
 		});
 	},
 
 	createMircoPoloFile: function(path, cb){
-		for( let i = 0 ;i < NO_OF_LINE ; i++){
+    // clear the db
+    MarcoPoloData.remove({}, (err) => {console.log(err);});
+		for( var i = 0 ;i < NO_OF_LINE ; i++){
 			var start = START + i*NO_LIMIT_IN_LINE;
 			var end = start + NO_LIMIT_IN_LINE ;
 			this.generate(start, end).then(function(data){
-				fs.appendFile(path, '\n'+data, function(error) {
-					if (error) {
-					  console.error("write error:  " + error.message);
-					  return cb(error);
-					} 
-				});
+        MarcoPoloData({
+          numbers: data.range,
+          result: data.result
+        }).save(function(err) {console.log(err)});
 			});
 		}
 		cb(null);
